8cb4b276c204f03a6e60ddc257b93ccff3063cc5
ginxwar <kevin.wu@gmail.com>
2014-11-10 21:37:21 -0800

    spots created from the spot menu now default with lat/lng textboxes available

--- www/js/controllers/spots.js
+++ www/js/controllers/spots.js
@@ -1,12 +1,21 @@
 angular.module('app')
 
-  .controller('SpotsCtrl', function($scope, $location, Spots, NewSpot) {
-    // Load or initialize Spots
-    $scope.spots = Spots.all();
+.controller('SpotsCtrl', function($scope, $location, Spots, NewSpot) {
+  // Load or initialize Spots
+  $scope.spots = Spots.all();
 
-    // Create a new Spot
-    $scope.newSpot = function() {
-      NewSpot.setNewSpot();
-      $location.path("/app/spots/newspot");
-    };
-  });
+  // a geojson template we pass in when creating a new spot from the spot menu
+  var geojsonTemplate = {
+    "geometry": {
+      "type": "Point",
+      "coordinates": [0, 0]
+    }
+  };
+
+  // Create a new Spot
+  $scope.newSpot = function() {
+    // we create a new spot and pass in the template because a new spot created in this manner is always a single point
+    NewSpot.setNewSpot(geojsonTemplate);
+    $location.path("/app/spots/newspot");
+  };
+});
