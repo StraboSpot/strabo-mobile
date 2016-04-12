686a611c66c8b099c9cb2d75c7befe567552b883
jessica-azgs <jessica.good@azgs.az.gov>
2016-04-12 16:10:42 -0700

    deploy update check message when unable to check

--- www/app/about/about.controller.js
+++ www/app/about/about.controller.js
@@ -11,11 +11,11 @@
     var vm = this;
     var deploy = new Ionic.Deploy();
 
-    vm.checkedForUpdates = false;
     vm.checkForUpdates = checkForUpdates;
     vm.doUpdate = doUpdate;
     vm.hasUpdate = false;
     vm.isWeb = typeof cordova === 'undefined';
+    vm.msg = undefined;
 
     /**
      * Public Functions
@@ -30,11 +30,14 @@
       vm.checkedForUpdates = false;
       deploy.check().then(function (hasUpdate) {
         $log.log('Ionic Deploy: Update available: ' + hasUpdate);
-        vm.checkedForUpdates = true;
         vm.hasUpdate = hasUpdate;
+        if (!vm.hasUpdate) vm.msg = 'No Update Available';
+        else vm.msg = undefined;
         $ionicLoading.hide();
         $scope.$apply();
       }, function (err) {
+        vm.msg = 'Unable to check for updates';
+        $scope.$apply();
         $ionicLoading.hide();
         $log.error('Ionic Deploy: Unable to check for updates', err);
       });
