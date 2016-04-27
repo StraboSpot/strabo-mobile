4e4ddc3e683e18ea8f3589b0e62bfa448d33215d
jessica-azgs <jessica.good@azgs.az.gov>
2016-04-27 13:40:33 -0700

    added statusbar as plugin in www/config.xml

--- www/app/app.run.js
+++ www/app/app.run.js
@@ -9,6 +9,11 @@
         // for form inputs)
         if ($window.cordova && $window.cordova.plugins.Keyboard) {
           cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+          cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if ($window.StatusBar) {
+          // org.apache.cordova.statusbar required
+          StatusBar.styleDefault();
         }
       });
     });
