2576728234af3bf947a50f003aac149e46da4e38
jessica-azgs <jessica.good@azgs.az.gov>
2016-04-27 15:56:48 -0700

    statusbar

--- www/app/app.run.js
+++ www/app/app.run.js
@@ -13,7 +13,7 @@
         }
         if ($window.StatusBar) {
           // org.apache.cordova.statusbar required
-          $window.StatusBar.styleDefault();
+          $window.StatusBar.styleBlackOpaque();
           $window.StatusBar.overlaysWebView(false);
         }
       });
