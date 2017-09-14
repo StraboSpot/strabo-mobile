e68f75737921c797f933a0d49b2f7fd81f3676d9
Jessica Good <jessicagood012@gmail.com>
2017-09-13 22:03:19 -0700

    hide export to csv button until csv converter working properly

--- www/app/spots/spots.controller.js
+++ www/app/spots/spots.controller.js
@@ -64,6 +64,69 @@
       createPageEvents()
     }
 
+    // Convert the spot objects to a csv format
+    function convertToCSV() {
+      // Get all the fields for the csv header row
+      var allHeaders = [];
+      _.each(vm.spots, function (spot) {
+        var headers = _.keys(spot.properties);
+        // If there are custom fields, loop through the custom object grabbing those fields
+        var i = _.indexOf(headers, 'custom');
+        if (i !== -1) {
+          headers = _.without(headers, 'custom');
+          var customHeaders = _.keys(spot.properties.custom);
+          customHeaders = _.map(customHeaders, function (header) {
+            return 'custom_' + header;
+          });
+          allHeaders = _.union(headers, allHeaders, customHeaders);
+        }
+        else allHeaders = _.union(headers, allHeaders);
+      });
+
+      // Add the two fields for the geometry
+      allHeaders.push('geometry_type');
+      allHeaders.push('geometry_coordinates');
+
+      var allHeadersQuoted = _.map(allHeaders, function (ele) {
+        return '\'' + ele + '\'';
+      });
+      var csv = allHeadersQuoted.toString() + '\r\n';
+
+      // Get all the values for each csv data row
+      _.each(vm.spots, function (spot) {
+        var row = new Array(allHeaders.length);
+        row = _.map(row, function (ele) {
+          return '';
+        });
+        _.each(spot.properties, function (value, key) {
+          // If the value is actually an object
+          var i = _.indexOf(allHeaders, key);
+          if (_.isObject(value)) {
+            // Separate date parts
+            if (value instanceof Date) {
+              if (key === 'time') row[i] = value.toLocaleTimeString();
+              else if (key === 'date') row[i] = value.toLocaleDateString();
+              else row[i] = value.toJSON();
+            }
+            // Flatten the child values
+            else row[i] = '\'' + _.flatten(value).join(', ') + '\'';
+          }
+          else row[i] = '\'' + value + '\'';
+        });
+        // Add the values for the geometry fields
+        if (angular.isDefined(spot.geometry) && angular.isDefined(spot.geometry.type)) {
+          var i = _.indexOf(allHeaders, 'geometry_type');
+          row[i] = '\'' + spot.geometry.type + '\'';
+          if (angular.isDefined(spot.geometry.coordinates)) {
+            i = _.indexOf(allHeaders, 'geometry_coordinates');
+            row[i] = '\'' + _.flatten(spot.geometry.coordinates).join(', ') + '\'';
+          }
+        }
+        csv += row.toString() + '\r\n';
+      });
+      return csv;
+    }
+
     function createPageComponents() {
       SpotsFactory.createSpotsFilterModal($scope).then(function (modal) {
         filterModal = modal
@@ -181,92 +244,6 @@
     function exportToCSV() {
       vm.popover.hide();
 
-      // Convert the spot objects to a csv format
-      function convertToCSV() {
-        // Get all the fields for the csv header row
-        var allHeaders = [];
-        _.each(vm.spots,
-          function (spot) {
-            var headers = _.keys(spot.properties);
-            // If there are custom fields, loop through the custom object grabbing those fields
-            var i = _.indexOf(headers, 'custom');
-            if (i !== -1) {
-              headers = _.without(headers, 'custom');
-              var customHeaders = _.keys(spot.properties.custom);
-              customHeaders = _.map(customHeaders,
-                function (header) {
-                  return 'custom_' + header;
-                }
-              );
-              allHeaders = _.union(headers, allHeaders, customHeaders);
-            }
-            else {
-              allHeaders = _.union(headers, allHeaders);
-            }
-          }
-        );
-        // Add the two fields for the geometry
-        allHeaders.push('geometry_type');
-        allHeaders.push('geometry_coordinates');
-
-        var allHeadersQuoted = _.map(allHeaders,
-          function (ele) {
-            return '\'' + ele + '\'';
-          }
-        );
-        var csv = allHeadersQuoted.toString() + '\r\n';
-
-        // Get all the values for each csv data row
-        _.each(vm.spots,
-          function (spot) {
-            var row = new Array(allHeaders.length);
-            row = _.map(row,
-              function (ele) {
-                return '';
-              }
-            );
-            _.each(spot.properties,
-              function (value, key) {
-                // If the value is actually an object
-                var i = _.indexOf(allHeaders, key);
-                if (_.isObject(value)) {
-                  // Separate date parts
-                  if (value instanceof Date) {
-                    if (key === 'time') {
-                      row[i] = value.toLocaleTimeString();
-                    }
-                    else if (key === 'date') {
-                      row[i] = value.toLocaleDateString();
-                    }
-                    else {
-                      row[i] = value.toJSON();
-                    }
-                  }
-                  // Flatten the child values
-                  else {
-                    row[i] = '\'' + _.flatten(value).join(', ') + '\'';
-                  }
-                }
-                else {
-                  row[i] = '\'' + value + '\'';
-                }
-              }
-            );
-            // Add the values for the geometry fields
-            if (angular.isDefined(spot.geometry) && angular.isDefined(spot.geometry.type)) {
-              var i = _.indexOf(allHeaders, 'geometry_type');
-              row[i] = '\'' + spot.geometry.type + '\'';
-              if (angular.isDefined(spot.geometry.coordinates)) {
-                i = _.indexOf(allHeaders, 'geometry_coordinates');
-                row[i] = '\'' + _.flatten(spot.geometry.coordinates).join(', ') + '\'';
-              }
-            }
-            csv += row.toString() + '\r\n';
-          }
-        );
-        return csv;
-      }
-
       var spotData = convertToCSV(vm.spots);
 
       if (IS_WEB) {
@@ -275,62 +252,60 @@
         win.document.body.innerHTML = spotData;
         return;
       }
+      try {
+        var d = new Date();
+        d = d.toLocaleDateString() + '-' + d.toLocaleTimeString();
+        d = d.replace(/\//g, '-');
+        d = d.replace(/:/g, '');
+        d = d.replace(/ /g, '');
+        var fileName = d + '-' + 'strabo-data.csv';
 
-      var d = new Date();
-      d = d.toLocaleDateString() + '-' + d.toLocaleTimeString();
-      d = d.replace(/\//g, '-');
-      d = d.replace(/:/g, '');
-      d = d.replace(/ /g, '');
-      var fileName = d + '-' + 'strabo-data.csv';
+        var devicePath;
+        switch ($cordovaDevice.getPlatform()) {
+          case 'Android':
+            devicePath = cordova.file.externalRootDirectory;
+            break;
+          case 'iOS':
+            devicePath = cordova.file.documentsDirectory;
+            break;
+          default:
+            // uh oh?  TODO: what about windows and blackberry?
+            devicePath = cordova.file.externalRootDirectory;
+            break;
+        }
 
-      var devicePath;
-      switch ($cordovaDevice.getPlatform()) {
-        case 'Android':
-          devicePath = cordova.file.externalRootDirectory;
-          break;
-        case 'iOS':
-          devicePath = cordova.file.documentsDirectory;
-          break;
-        default:
-          // uh oh?  TODO: what about windows and blackberry?
-          devicePath = cordova.file.externalRootDirectory;
-          break;
-      }
+        var directory = 'strabo';
 
-      var directory = 'strabo';
-
-      function writeFile(dir) {
-        $cordovaFile.writeFile(devicePath + dir, fileName, spotData, true).then(
-          function (success) {
+        function writeFile(dir) {
+          $cordovaFile.writeFile(devicePath + dir, fileName, spotData, true).then(function (success) {
             $log.log(success);
             $ionicPopup.alert({
               'title': 'Success!',
               'template': 'CSV written to ' + devicePath + dir + fileName
             });
-          },
-          function (error) {
-            $log.log(error);
-          }
-        );
-      }
+          }, function (error) {
+            $log.error('Error:', error);
+          });
+        }
 
-      $cordovaFile.checkDir(devicePath, directory).then(
-        function (success) {
+        $cordovaFile.checkDir(devicePath, directory).then(function (success) {
           $log.log(success);
           writeFile(success.fullPath);
-        },
-        function (error) {
+        }, function (error) {
           $cordovaFile.createDir(devicePath, directory, false).then(
             function (success) {
               $log.log(success);
               writeFile(success.fullPath);
             },
             function (error) {
-              $log.log(error);
-            }
-          );
-        }
-      );
+              $log.error('Error:', error);
+            });
+        });
+      }
+      catch (err) {
+        if (err.message) $log.error('Error:', err.message);
+        else $log.error('Error');
+      }
     }
 
     function getTagNames(spotId) {
