8e89b74ceda6e32abc24ed936bbf0dcc9b6e92d8
Jessica Good <jessicagood012@gmail.com>
2018-04-02 15:43:36 -0700

    Web - move map menu buttons when side panel moves

--- www/app/maps/side-panel/side-panel.controller.js
+++ www/app/maps/side-panel/side-panel.controller.js
@@ -5,9 +5,9 @@
     .module('app')
     .controller('MapSidePanelController', MapSidePanelController);
 
-  MapSidePanelController.$inject = ['$rootScope', '$scope', 'SpotFactory'];
+  MapSidePanelController.$inject = ['$document', '$rootScope', '$scope', '$window', 'SpotFactory'];
 
-  function MapSidePanelController($rootScope, $scope, SpotFactory) {
+  function MapSidePanelController($document, $rootScope, $scope, $window, SpotFactory) {
     var vm = this;
 
     var activeTab = 'spot';
@@ -27,10 +27,36 @@
         SpotFactory.setCurrentSpotById(clickedFeatureId);
         $rootScope.$broadcast('load-tab', {'spotId': clickedFeatureId, 'tabName': activeTab });
       });
+
+      moveIcons(true);
+      var toggle = $document[0].getElementsByClassName('toggler')[0];
+      toggle.addEventListener('click', function (evt) {
+        moveIcons();
+      });
     }
 
     function isActiveTab(tab) {
       return tab === activeTab;
+    }
+
+    function moveIcons(isLoading) {
+      var sidePanelNode  = $document[0].getElementById('map-side-panel');
+      var sidePanelRightPosition = $window.getComputedStyle(sidePanelNode, null).getPropertyValue('right');
+      sidePanelRightPosition = parseInt(sidePanelRightPosition);
+      var sidePanelLeftPosition = Math.abs(sidePanelRightPosition);
+
+      // On loading emogeo buttons and map menu buttons are positioned after side panel
+      // If not loading these buttons are positioned before side panel toggle complete
+      if (isLoading && sidePanelRightPosition < 0) sidePanelLeftPosition = 0;
+      else if (isLoading) sidePanelLeftPosition = sidePanelNode.offsetWidth;
+
+      var mapMenuNode = $document[0].getElementsByClassName('map-menu')[0];
+      if (sidePanelLeftPosition === 0) {
+        mapMenuNode.style.right = "10px";
+      }
+      else {
+        mapMenuNode.style.right = sidePanelLeftPosition + 10 + "px";
+      }
     }
 
     function onLoad(tab) {
