5a314281fbe4c35a9353450d65f8c7918a6c01aa
Nathan Novak <nnovak@ku.edu>
2018-08-08 17:13:28 -0700

    Micro: data page construct

--- www/app/spot/data/data-tab.controller.js
+++ www/app/spot/data/data-tab.controller.js
@@ -13,6 +13,13 @@
 
     var thisTabName = 'data';
 
+    vm.url = '';
+    vm.urlArr = [];
+
+    vm.deleteLink = deleteLink;
+    vm.editLink = editLink;
+    vm.urlLinkSubmit = urlLinkSubmit;
+
     activate();
 
     /**
@@ -47,5 +54,69 @@
      * Public Functions
      */
 
+    function deleteLink(urlToDelete) {
+      var confirmPopup = $ionicPopup.confirm({
+        'title': 'Delete Link',
+        'template': 'Are you sure you want to delete the link <b>' + urlToDelete + '</b>?'
+      });
+      confirmPopup.then(function (res) {
+        if (res) {
+          vmParent.spot.properties.data.links = _.without(vmParent.spot.properties.data.links, urlToDelete);
+          if (vmParent.spot.properties.data.links.length === 0) delete vmParent.spot.properties.data.links;
+          if (_.isEmpty(vmParent.spot.properties.data)) delete vmParent.spot.properties.data;
+          vmParent.saveSpot().then(function () {
+            vmParent.spotChanged = false;
+          });
+        }
+      });
+    }
+
+     // rename url
+    function editLink(urlToEdit) {
+      var myPopup = $ionicPopup.show({
+        'template': '<input type="url" placeholder="http://" ng-model="vmChild.url">',
+        'title': 'Enter URL',
+        'scope': $scope,
+        'buttons': [
+          {'text': 'Cancel'},
+          {
+            'text': '<b>Save</b>',
+            'type': 'button-positive',
+            'onTap': function (e) {
+              if (!vm.url) e.preventDefault();
+              else return vm.url;
+            }
+          }
+        ]
+      });
+
+      myPopup.then(function (url) {
+        if (url) {
+          vmParent.spot.properties.data.links = _.without(vmParent.spot.properties.data.links, urlToEdit);
+          vmParent.spot.properties.data.links.push(url);
+          vmParent.saveSpot().then(function () {
+            vmParent.spotChanged = false;
+            vm.url = '';
+          });
+        }
+      });
+    }
+
+    function urlLinkSubmit() {
+      $log.log('Data URL', vm.url);
+      if (angular.isUndefined(vm.url)) {
+        $ionicPopup.alert({
+          'title': 'Invalid URL!',
+          'template': 'Please include "http://" or "https://" before entering website'}
+        );
+        vm.url = '';
+      }
+      else {
+        if (!vmParent.spot.properties.data) vmParent.spot.properties.data = {};
+        if (!vmParent.spot.properties.data.links) vmParent.spot.properties.data.links = [];
+        vmParent.spot.properties.data.links.push(vm.url);
+        vm.url = '';
+      }
+    }
   }
 }());
