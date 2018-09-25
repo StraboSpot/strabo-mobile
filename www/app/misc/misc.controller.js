8209674d2bc43ca6b558ff4e0f471f892d7ec51c
Nathan Novak <nnovak@ku.edu>
2018-09-25 15:07:02 -0700

    Data: put into testing mode

--- www/app/misc/misc.controller.js
+++ www/app/misc/misc.controller.js
@@ -213,6 +213,7 @@
         preferences.experimental = false;
         preferences.experimental_results = false;
         preferences.experimental_set_up = false;
+        preferences.data = false;
         ProjectFactory.saveProjectItem('preferences', preferences);
       }
     }
