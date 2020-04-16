2aea3a99c06db05e39b684c0a97417d1d223986e
Jessica Good <jessicagood012@gmail.com>
2020-04-16 15:38:09 -0700

    Spot Data Page: Allow csv import from device

--- www/app/spot/data/data-tab.controller.js
+++ www/app/spot/data/data-tab.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   angular
@@ -15,19 +15,16 @@
 
     var thisTabName = 'data';
 
-    vm.csv = '';
-    vm.csvArr = [];
-    vm.importItem = undefined;
+    vm.tableData = [];
     vm.url = '';
-    vm.urlArr = [];
 
-    vm.deleteCSV = deleteCSV;
-    vm.deleteLink = deleteLink;
-    vm.editLink = editLink;
+    vm.deleteTable = deleteTable;
+    vm.deleteURL = deleteURL;
+    vm.editURL = editURL;
     vm.importData = importData;
     vm.isWeb = isWeb;
-    vm.urlLinkSubmit = urlLinkSubmit;
-    vm.viewCSVFile = viewCSVFile;
+    vm.urlSubmit = urlSubmit;
+    vm.viewTable = viewTable;
 
     activate();
 
@@ -37,7 +34,7 @@
 
     function activate() {
       createModal();
-      if (IS_WEB) ionic.on('change', getFile, $document[0].getElementById('dataFile'));
+      ionic.on('change', getFile, $document[0].getElementById('dataFile'));
       $log.log('In DataTabController');
 
       // Loading tab from Spots list
@@ -77,24 +74,25 @@
         $ionicLoading.show({
           template: '<ion-spinner></ion-spinner><br>Loading File Please Wait...'
         });
-        var fileNameId = HelpersFactory.getNewId();
-        $log.log(fileNameId, fileName);
+        var id = HelpersFactory.getNewId();
+        $log.log(id, fileName);
         var csvToArr = HelpersFactory.csvToArray(file);
         $log.log('CSV to Array', csvToArr);
         var CSVObject = {
-          'fileNameId': fileNameId,
-          'fileName': fileName,
-          'file': csvToArr
+          'id': id,
+          'name': fileName,
+          'data': csvToArr
         };
         $log.log('CSVObject', CSVObject);
         if (!vmParent.spot.properties.data) vmParent.spot.properties.data = {};
-        if (!vmParent.spot.properties.data.csv) vmParent.spot.properties.data.csv = [];
-        vmParent.spot.properties.data.csv.push(CSVObject);
+        if (!vmParent.spot.properties.data.tables) vmParent.spot.properties.data.tables = [];
+        vmParent.spot.properties.data.tables.push(CSVObject);
       }
       else {
         var alertPopup = $ionicPopup.alert({
           'title': 'Not in CSV Format!',
-          'template': 'The filename: <br><b>' + fileName + '</b><br> is not in the correct format. <br>Please ensure that the filename ends is in .csv format.'
+          'template': 'The filename: <br><b>' + fileName + '</b><br> is not in the correct format. <br>Please ' +
+            'ensure that the filename ends with .csv.'
         });
 
         alertPopup.then(function (res) {
@@ -130,15 +128,15 @@
      * Public Functions
      */
 
-    function deleteCSV(CSVToDelete) {
+    function deleteTable(tableToDelete) {
       var confirmPopup = $ionicPopup.confirm({
-        'title': 'Delete CSV File?',
-        'template': 'Are you sure you want to delete the CSV file <b>' + CSVToDelete.fileName + '</b>?'
+        'title': 'Delete Table?',
+        'template': 'Are you sure you want to delete the table <b>' + tableToDelete.name + '</b>?'
       });
       confirmPopup.then(function (res) {
         if (res) {
-          vmParent.spot.properties.data.csv = _.without(vmParent.spot.properties.data.csv, CSVToDelete);
-          if (vmParent.spot.properties.data.csv.length === 0) delete vmParent.spot.properties.data.csv;
+          vmParent.spot.properties.data.tables = _.without(vmParent.spot.properties.data.tables, tableToDelete);
+          if (vmParent.spot.properties.data.tables.length === 0) delete vmParent.spot.properties.data.tables;
           if (_.isEmpty(vmParent.spot.properties.data)) delete vmParent.spot.properties.data;
           vmParent.saveSpot().then(function () {
             vmParent.spotChanged = false;
@@ -147,15 +145,15 @@
       });
     }
 
-    function deleteLink(urlToDelete) {
+    function deleteURL(urlToDelete) {
       var confirmPopup = $ionicPopup.confirm({
         'title': 'Delete Link?',
         'template': 'Are you sure you want to delete the link <b>' + urlToDelete + '</b>?'
       });
       confirmPopup.then(function (res) {
         if (res) {
-          vmParent.spot.properties.data.links = _.without(vmParent.spot.properties.data.links, urlToDelete);
-          if (vmParent.spot.properties.data.links.length === 0) delete vmParent.spot.properties.data.links;
+          vmParent.spot.properties.data.urls = _.without(vmParent.spot.properties.data.urls, urlToDelete);
+          if (vmParent.spot.properties.data.urls.length === 0) delete vmParent.spot.properties.data.urls;
           if (_.isEmpty(vmParent.spot.properties.data)) delete vmParent.spot.properties.data;
           vmParent.saveSpot().then(function () {
             vmParent.spotChanged = false;
@@ -165,7 +163,7 @@
     }
 
     // rename url
-    function editLink(urlToEdit) {
+    function editURL(urlToEdit) {
       var myPopup = $ionicPopup.show({
         'template': '<input type="url" placeholder="http://" ng-model="vmChild.url">',
         'title': 'Enter URL',
@@ -184,8 +182,8 @@
       });
       myPopup.then(function (url) {
         if (url) {
-          vmParent.spot.properties.data.links = _.without(vmParent.spot.properties.data.links, urlToEdit);
-          vmParent.spot.properties.data.links.push(url);
+          vmParent.spot.properties.data.urls = _.without(vmParent.spot.properties.data.urls, urlToEdit);
+          vmParent.spot.properties.data.urls.push(url);
           vmParent.saveSpot().then(function () {
             vmParent.spotChanged = false;
             vm.url = '';
@@ -196,14 +194,14 @@
 
     function importData() {
       vm.fileNames = [];
-      if (IS_WEB) $document[0].getElementById('dataFile').click();
+      $document[0].getElementById('dataFile').click();
     }
 
     function isWeb() {
       return IS_WEB;
     }
 
-    function urlLinkSubmit() {
+    function urlSubmit() {
       $log.log('Data URL', vm.url);
       if (angular.isUndefined(vm.url)) {
         $ionicPopup.alert({
@@ -214,16 +212,16 @@
       }
       else {
         if (!vmParent.spot.properties.data) vmParent.spot.properties.data = {};
-        if (!vmParent.spot.properties.data.links) vmParent.spot.properties.data.links = [];
-        vmParent.spot.properties.data.links.push(vm.url);
+        if (!vmParent.spot.properties.data.urls) vmParent.spot.properties.data.urls = [];
+        vmParent.spot.properties.data.urls.push(vm.url);
         vm.url = '';
       }
     }
 
-    function viewCSVFile(csvFile) {
-      // $log.log('csvFile', csvFile.file);
-      vm.csvArr = csvFile.file;
-      vm.currentFileTitle = csvFile.fileName;
+    function viewTable(tableToView) {
+      // $log.log('tableToView', tableToView);
+      vm.tableData = tableToView.data;
+      vm.currentTableTitle = tableToView.name;
       vm.dataTableModal.show();
     }
   }
